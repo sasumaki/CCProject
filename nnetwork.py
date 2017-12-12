83458726bb5b9a8fcb7e3085b136727320f036f2
Juho Lamminmäki <juho.lamminmaki@gmail.com>
2017-12-12 08:21:31 +0200

    Network with embeddings

--- nnetwork.py
+++ nnetwork.py
@@ -2,7 +2,7 @@ import glob
 import matplotlib as mpl
 # This line allows mpl to run with no DISPLAY defined
 import re
-from keras import Input, Model
+from keras import Input, Model, layers
 from tensorflow.python import debug as tf_debug
 import pprint
 
@@ -13,7 +13,7 @@ mpl.use('Agg')
 
 import numpy as np
 import os
-from keras.layers import Reshape, Flatten, LeakyReLU, Activation, Concatenate, K
+from keras.layers import Reshape, Flatten, LeakyReLU, Activation, Concatenate, K, Embedding
 from keras.layers.convolutional import UpSampling2D, MaxPooling2D
 from keras.models import Sequential, load_model
 from keras.optimizers import Adam
@@ -32,10 +32,12 @@ def model_generator():
     reg = lambda: l1l2(l1=1e-7, l2=1e-7)
     h = 5
 
-    input_z = Input(shape=(100,))
-    input_label = Input(shape=(10,))
+    input_z = Input(shape=(latent_dim,))
+    input_label = Input(shape=(1,))
 
-    H = Concatenate()([input_z, input_label])
+    input_label_embedding = Flatten()(Embedding(10, latent_dim, embeddings_initializer='glorot_normal')(input_label))
+
+    H = layers.multiply([input_z, input_label_embedding])
     H = Dense(nch * 4 * 4, W_regularizer=reg())(H)
     H = BatchNormalization(mode=0)(H)
     H = Reshape(dim_ordering_shape((nch, 4, 4)))(H)
@@ -71,7 +73,6 @@ def model_discriminator():
     c2 = Convolution2D(int(nch / 4), h, h, border_mode='same', W_regularizer=reg())
     c3 = Convolution2D(int(nch / 2), h, h, border_mode='same', W_regularizer=reg())
     c4 = Convolution2D(int(nch), h, h, border_mode='same', W_regularizer=reg())
-    c5 = Convolution2D(1, h, h, border_mode='same', W_regularizer=reg())
 
     H = c1(input_d)
     H = MaxPooling2D(pool_size=(2, 2))(H)
@@ -97,6 +98,7 @@ def model_discriminator():
     category = Activation('softmax')(category)
     return Model(inputs=[input_d], outputs=[fake, category])
 
+
 def build_gan(generator, discriminator, name="gan"):
     """
     Build GAN from generator and discriminator
@@ -116,13 +118,15 @@ def build_gan(generator, discriminator, name="gan"):
 def simple_gan(generator, discriminator, latent_sampling):
     return build_gan(generator, discriminator)
 
+
 optimizer = AdversarialOptimizerSimultaneous()
 opt_g = Adam(1e-5, decay=1e-5)
 opt_d = Adam(5e-5, decay=1e-5)
 nb_epoch = 500
-path = os.path.join("output", "art7")
+path = os.path.join("output", "art9")
 loss = 'binary_crossentropy'
 latent_dim = 100
+
 
 def main():
     # Uncomment this for debugging
@@ -142,26 +146,7 @@ def main():
     one_hots = np.eye(one_hot_length)[targets]
     one_hots = one_hots[imageloader.painting_filter()]
 
-    generators = glob.glob(path + "/**/generator.*")
-    discriminators = glob.glob(path + "/**/discriminator.*")
-
-    current_epoch = 1
-    if generators:
-        latest_generator = max(generators, key=os.path.getctime)
-        generator = load_model(latest_generator)
-        current_epoch = max(current_epoch, 1 + int(re.search("generator\.(\d*)\.h5", latest_generator).group(1)))
-    else:
-        generator = model_generator()
-        current_epoch = max(current_epoch, 1)
-
-    if discriminators:
-        latest_discriminator = max(discriminators, key=os.path.getctime)
-        discriminator = load_model(latest_discriminator)
-        current_epoch = max(current_epoch, 1 + int(re.search("discriminator\.(\d*)\.h5", latest_discriminator).group(1)))
-    else:
-        discriminator = model_discriminator()
-        current_epoch = max(current_epoch, 1)
-
+    current_epoch, discriminator, generator = load_existing_models()
 
     generator.summary()
     discriminator.summary()
@@ -180,14 +165,36 @@ def main():
                                   "yreal": "binary_crossentropy",
                                   "yreal_label": "categorical_crossentropy",
                                   "yfake_label": "categorical_crossentropy"
-                              })
+                                  })
 
     callbacks = initialize_callbacks(path, generator, discriminator, latent_dim)
 
-    y = y[:2] + [one_hots]*2 + y[2:] + [one_hots]*2
-    history = fit(model, x=[z, one_hots, xtrain], y=y,
+    y = y[:2] + [one_hots] * 2 + y[2:] + [one_hots] * 2
+    history = fit(model, x=[z, numerical_categories[imageloader.painting_filter()], xtrain], y=y,
                   callbacks=callbacks, nb_epoch=nb_epoch, initial_epoch=current_epoch,
                   batch_size=32)
+
+
+def load_existing_models():
+    generators = glob.glob(path + "/**/generator.*")
+    discriminators = glob.glob(path + "/**/discriminator.*")
+    current_epoch = 1
+    if generators:
+        latest_generator = max(generators, key=os.path.getctime)
+        generator = load_model(latest_generator)
+        current_epoch = max(current_epoch, 1 + int(re.search("generator\.(\d*)\.h5", latest_generator).group(1)))
+    else:
+        generator = model_generator()
+        current_epoch = max(current_epoch, 1)
+    if discriminators:
+        latest_discriminator = max(discriminators, key=os.path.getctime)
+        discriminator = load_model(latest_discriminator)
+        current_epoch = max(current_epoch,
+                            1 + int(re.search("discriminator\.(\d*)\.h5", latest_discriminator).group(1)))
+    else:
+        discriminator = model_discriminator()
+        current_epoch = max(current_epoch, 1)
+    return current_epoch, discriminator, generator
 
 
 class AdversarialModelSaver(Callback):
@@ -205,9 +212,9 @@ class AdversarialModelSaver(Callback):
 
 def initialize_callbacks(path, generator, discriminator, latent_dim):
     def generator_sampler():
-        hots = np.eye(10)[[int(t / 10) for t in range(100)]]
+        labels = np.array([int(t / 10) for t in range(100)])
         zsamples = np.random.normal(size=(10 * 10, latent_dim))
-        generated_images = generator.predict([zsamples, hots])
+        generated_images = generator.predict([zsamples, labels])
         xpred = dim_ordering_unfix(generated_images).transpose((0, 2, 3, 1))
 
         pprint.pprint(discriminator.predict(generated_images))
@@ -220,5 +227,7 @@ def initialize_callbacks(path, generator, discriminator, latent_dim):
 
     callbacks = [generator_cb, tensor_board, model_saver]
     return callbacks
+
+
 if __name__ == "__main__":
     main()
